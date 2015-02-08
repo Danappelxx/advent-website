3627cd6fe54a5c8386e6f2e5cfeb68ff200b2d1a
NPN <npn.git@gmail.com>
2015-02-07 21:13:52 -0800

    event loading code

--- js/event_list.js
+++ js/event_list.js
@@ -6,7 +6,7 @@ function createListItem(eventID, name, thumbnailURL, desc, photoCount, owner){
     var template = $("#template").clone();
     template.removeAttr("id");
     template.find(".media-object").attr("src", thumbnailURL);
-    template.find(".media-heading").innerHTML=name;
+    template.find(".media-heading").text(name);
     template.find("#desc").text(desc);
     var eventPageURL = "event.html?id=";
     template.find("a").attr("href", eventPageURL + eventID);
